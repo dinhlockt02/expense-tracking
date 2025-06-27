78929e2964d31eb943678e5bace8dbbc7a134042
Tran Dinh Loc <dinhlockt02@gmail.com>
2025-06-27 19:14:03 +0700

    feat: implement presignup lambda trigger

---
+++ infra/aws/lambda/endpoint/pre_signup_trigger/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"errors"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, event events.CognitoEventUserPoolsPreSignup) (events.CognitoEventUserPoolsPreSignup, error) {
+	if event.TriggerSource != "PreSignUp_ExternalProvider" {
+		return handlingFederatedSignUp(event)
+	}
+
+	return event, nil
+}
+
+func handlingFederatedSignUp(event events.CognitoEventUserPoolsPreSignup) (events.CognitoEventUserPoolsPreSignup, error) {
+	email := event.Request.UserAttributes["email"]
+
+	// Check if the email is already registered
+	sess := session.Must(session.NewSession())
+	svc := cognitoidentityprovider.New(sess)
+
+	// Search for existing users with the same email
+	input := &cognitoidentityprovider.ListUsersInput{
+		UserPoolId: &event.UserPoolID,
+		Filter:     aws.String("email = \"" + email + "\""),
+		Limit:      aws.Int64(1),
+	}
+
+	users, err := svc.ListUsers(input)
+	if err != nil {
+		return event, err
+	}
+	if len(users.Users) != 0 {
+		return event, errors.New("email already registered")
+	}
+
+	// Allow sign-up for users coming from an external provider
+	event.Response.AutoConfirmUser = true
+	event.Response.AutoVerifyEmail = true
+	event.Response.AutoVerifyPhone = false // Assuming phone verification is not required for external providers
+
+	return event, nil
+}
---
+++ infra/aws/lambda/go.mod
@@ -0,0 +1,9 @@
+module github.com/dinhlockt02/expense-tracking/infra/aws/lambda
+
+go 1.24.4
+
+require (
+	github.com/aws/aws-lambda-go v1.49.0 // indirect
+	github.com/aws/aws-sdk-go v1.55.7 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
